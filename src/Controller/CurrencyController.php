07039cfb02f9a8d90f29df75446942b1322fff70
Gabriela Lubkowska <dsw45017@student.dsw.edu.pl>
2023-03-30 21:54:42 +0200

    Add currency controller

---
+++ src/Controller/CurrencyController.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\Currency;
+use App\Service\CurrencyService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class CurrencyController extends AbstractController
+{
+    public function __construct(private CurrencyService $currencyService) { }
+
+    #[Route('/', name: 'currency_list')]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $currencies = $entityManager->getRepository(Currency::class)->findAll();
+        return $this->render('currency/index.html.twig', [
+            'currencies' => $currencies,
+        ]);
+    }
+
+    #[Route('/currency/update-exchange-rates', name: 'currency_update_exchange_rates')]
+    public function updateExchangeRates(): Response
+    {
+        $this->currencyService->updateExchangeRates();
+
+        return new Response('Exchange rates updated!');
+    }
+}
